256ca6c008d98ccef15fdabd92247ad57a86d034
Harshit <ht2995@gmail.com>
2018-09-06 22:51:16 +0530

    class and asm.jar

---
+++ main.java
@@ -0,0 +1,14 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		add(2,3);
+	}
+
+	public static void add(int i, int j) {
+		// TODO Auto-generated method stub
+		System.out.println(i+j);
+	}
+
+}
